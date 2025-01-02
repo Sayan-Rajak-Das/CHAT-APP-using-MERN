9db8bf547c2b79d3b63e6106a556b16ade0ba73f
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2025-01-02 14:18:41 +0530

    Implement the profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,11 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+// Increase the payload size limit
+app.use(express.json({ limit: "10mb" })); // Set the limit to 10MB or more as per your needs
+app.use(express.urlencoded({ limit: "10mb", extended: true })); // For form data
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
