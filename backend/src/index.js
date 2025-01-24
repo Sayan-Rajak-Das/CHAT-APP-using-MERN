0e72638bdfc30b193f7fb65905ab554836465c6d
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2025-01-24 17:58:14 +0530

    For Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Increase the payload size limit
 app.use(express.json({ limit: "10mb" })); // Set the limit to 10MB or more as per your needs
@@ -24,6 +27,16 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 server.listen(PORT, () =>{
     console.log("Server is running on the port", PORT);
